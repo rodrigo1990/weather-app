adde86ec5fb99054482d3389062fc4ff6a936d02
rodrigo1990 <mcd77.1990@gmail.com>
2022-03-14 00:31:21 +0100

    269. hacemos funcionar ErrorBoundary

--- src/generic/ErrorBoundary/ErrorBoundary.jsx
+++ src/generic/ErrorBoundary/ErrorBoundary.jsx
@@ -6,48 +6,23 @@ class ErrorBoundary extends Component {
     constructor(props){
         super(props)
         this.state = {
-            activo: false
+            hasError: false
         }
     }
+
+    // this.setState( getDerivedStateFromError( error ) )
+    static getDerivedStateFromError( error ) {
+        
+        return { hasError: true }
+    }
     
-    estaActivo = () => {
-        return this.state.activo ? "Activo" : "No activo"
-    }
-
-    onClickHandler = () => {
-        //setState
-        //this.state.activo = true
-        this.setState({activo: !this.state.activo}) // el estado siempre se cambia a travez de setState
-    }
-
-    componentDidMount() {
-        console.log('El componente se ha montado')
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        console.log('estado previo:', prevState.activo)
-        console.log('Estado actual: ', this.state.activo)
-        console.log('El componente se ha actualizo')
-    }
-
-    componentWillUnmount() {
-        console.log('El component se ha desmontado')
-    }
     
     render(){// metodo sin parametros
         return (
-            <div>
-                <button onClick={this.onClickHandler}>
-                    Activar
-                </button>
-                <h1> 
-                    ErrorBoundary {this.props.saludo}
-                    {
-                        this.estaActivo()
-                    }        
-                </h1>
-            </div>
-            
+            this.state.hasError ? 
+                (<h1>Hubo un error</h1>)
+            :
+               ( this.props.children)
         )
     }
 }
--- src/generic/ErrorBoundary/ErrorBoundary.stories.jsx
+++ src/generic/ErrorBoundary/ErrorBoundary.stories.jsx
@@ -7,4 +7,25 @@ export default {
     component: ErrorBoundary
 }
 
-export const ErrorBoundaryExample = () => <ErrorBoundary saludo="hola"/>
+
+
+const ComponentWithoutError = () => <h1>Sin error</h1>
+
+const prop = undefined
+const ComponentWithError = () => <h1>{prop.hola}</h1>
+
+
+export const ErrorBoundaryWithError = () => (
+    <ErrorBoundary>
+        <ComponentWithError />
+    </ErrorBoundary>
+)
+
+
+
+
+export const ErrorBoundaryWithoutError = () => (
+    <ErrorBoundary>
+        <ComponentWithoutError />
+    </ErrorBoundary>
+)
