7c94c3cad1f174acb60fe5d7ff349d0460199c42
rodrigo1990 <mcd77.1990@gmail.com>
2022-03-13 23:55:20 +0100

    264. component did update

--- src/generic/ErrorBoundary/ErrorBoundary.jsx
+++ src/generic/ErrorBoundary/ErrorBoundary.jsx
@@ -17,8 +17,20 @@ class ErrorBoundary extends Component {
     onClickHandler = () => {
         //setState
         //this.state.activo = true
-        this.setState({activo: true}) // el estado siempre se cambia a travez de setState
+        this.setState({activo: !this.state.activo}) // el estado siempre se cambia a travez de setState
     }
+
+    componentDidMount() {
+        console.log('El componente se ha montado')
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('estado previo:', prevState.activo)
+        console.log('Estado actual: ', this.state.activo)
+        console.log('El componente se ha actualizo')
+    }
+    
+    
 
     
     render(){// metodo sin parametros
