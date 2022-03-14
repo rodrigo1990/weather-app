899f13f3f43b55f9f97baa18bdb79ff17fdc1fa7
rodrigo1990 <mcd77.1990@gmail.com>
2022-03-14 19:27:13 +0100

    283. Localizacion de Moment

--- src/pages/CityPage.jsx
+++ src/pages/CityPage.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import Grid from '@material-ui/core/Grid'
 import moment from 'moment'
+import 'moment/locale/es'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
 import AppFrame from './../components/AppFrame'
