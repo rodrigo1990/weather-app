aad8dcf89e09f9d6b8f12bd1ad0cf54da16fe57a
rodrigo1990 <mcd77.1990@gmail.com>
2022-03-15 23:58:35 +0100

    292. Creamos custom hook useCityList

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -1,15 +1,18 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import axios from 'axios'
-import CityInfo from '../CityInfo'
+
 import Alert  from '@mui/material/Alert'
-import convertUnits from 'convert-units'
 import Grid from '@material-ui/core/Grid'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
+import CityInfo from '../CityInfo'
+import useCityList from '../../hooks/useCityList'
 import Weather from '../Weather'
+import {getCityCode} from '../../utils/utils'
 
-const getCityCode = (city, countryCode) => `${city}-${countryCode}`
+
+
+
 //renderCityAndCountry se va a convertir en una funcion que retorna otra funcion
 const renderCityAndCountry = eventOnClickCity => (cityAndCountry, weather) => {
     
@@ -51,56 +54,10 @@ const renderCityAndCountry = eventOnClickCity => (cityAndCountry, weather) => {
 }
 
 const CityList = ({ cities, onClickCity }) => {
-    /* allWeather
-    
-        [Buenos Aires-argentina]: {temperature, 10, state: "sunny"}
-        [Formosa-Argentina]: {temperature, 10, state: "sunny"},
-        [Medellin-Colombia]: {temperature, 10, state: "sunny" }
-    */
-    const [allWeather, setAllWeather]   = useState({})
-    const [error, setError] = useState(null)
+ 
+    const { error, setError, allWeather } = useCityList(cities)
 
-
-    useEffect(() => {
-
-        const setWeather =  async (city, countryCode) => {
-
-            const apiID = "67e480613b70c9ce77febdb75c011b3e"
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&&appid=${apiID}` 
-            
-            try {
-                const response = await axios.get(url)
-                const { data } = response
-                const temperature = Number(convertUnits(data.main.temp).from('K').to('C').toFixed(0))
-                const state = data.weather[0].main.toLowerCase()
-
-                const propName = getCityCode(city, countryCode) //Ej: [Buenos Aires-argentina] ==> INDICE
-                const propValue = { temperature, state } //Ej: {temperature, 10, state: "sunny" } ==> VALUE
-                
-                //set[VARIABLE_ESTADO](VIARABLE_ESTADO => VARIABLE_ESTADO+1)
-                setAllWeather(allWeather => ({...allWeather,[propName] : propValue}))//deestructuring para agregar valores en objeto en una sola linea    
-            } catch (error) {
-                //Errores que nos responde el servidor
-                if( error.response ){
-                    setError("Ha ocurrido un error en el servidor del clima") 
-                }else if(error.request){//Errores que suceden por no llegar al server
-                    setError("Verifique la conexion a internet")
-                }else{//Errores imprevisibles
-                    setError("Error al cargar los datos")
-                }
-            }
-        }
-
-        //deestrucuting
-        cities.forEach( ( { city, countryCode } ) =>{
-            setWeather(city,countryCode)
-        });
-        
-
-    }, [cities])//[cities] ==> dependencia!, allWeather deberia ser una dependencia pero lo usamos de tal manera en el setAllWeather que no nescesario hacerlo
-
-//    const weather = { temperature:10, state:"sunny" }
-
+    //    const weather = { temperature:10, state:"sunny" }
     return (
         <div>
             {
---
+++ src/hooks/useCityList.jsx
@@ -0,0 +1,60 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+import convertUnits from 'convert-units'
+import {getCityCode} from '../utils/utils'
+
+const useCityList = (cities) => {
+    /* allWeather
+ 
+     [Buenos Aires-argentina]: {temperature, 10, state: "sunny"}
+     [Formosa-Argentina]: {temperature, 10, state: "sunny"},
+     [Medellin-Colombia]: {temperature, 10, state: "sunny" }
+ */
+     const [allWeather, setAllWeather]   = useState({})
+     const [error, setError] = useState(null)
+ 
+ 
+     useEffect(() => {
+ 
+         const setWeather =  async (city, countryCode) => {
+ 
+             const apiID = "67e480613b70c9ce77febdb75c011b3e"
+             const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&&appid=${apiID}` 
+             
+             try {
+                 const response = await axios.get(url)
+                 const { data } = response
+                 const temperature = Number(convertUnits(data.main.temp).from('K').to('C').toFixed(0))
+                 const state = data.weather[0].main.toLowerCase()
+ 
+                 const propName = getCityCode(city, countryCode) //Ej: [Buenos Aires-argentina] ==> INDICE
+                 const propValue = { temperature, state } //Ej: {temperature, 10, state: "sunny" } ==> VALUE
+                 
+                 //set[VARIABLE_ESTADO](VIARABLE_ESTADO => VARIABLE_ESTADO+1)
+                 setAllWeather(allWeather => ({...allWeather,[propName] : propValue}))//deestructuring para agregar valores en objeto en una sola linea    
+             } catch (error) {
+                 //Errores que nos responde el servidor
+                 if( error.response ){
+                     setError("Ha ocurrido un error en el servidor del clima") 
+                 }else if(error.request){//Errores que suceden por no llegar al server
+                     setError("Verifique la conexion a internet")
+                 }else{//Errores imprevisibles
+                     setError("Error al cargar los datos")
+                 }
+             }
+         }
+ 
+         //deestrucuting
+         cities.forEach( ( { city, countryCode } ) =>{
+             setWeather(city,countryCode)
+         });
+         
+ 
+     }, [cities])//[cities] ==> dependencia!, allWeather deberia ser una dependencia pero lo usamos de tal manera en el setAllWeather que no nescesario hacerlo
+
+
+     return {error,setError, allWeather}
+}
+
+
+export default useCityList
