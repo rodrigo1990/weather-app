60a33d43d2b4596200771034f8010066971c2dea
rodrigo1990 <mcd77.1990@gmail.com>
2022-03-16 16:31:27 +0100

    303. Funcion de transformacion getChartData

--- src/hooks/useCityPage.jsx
+++ src/hooks/useCityPage.jsx
@@ -4,6 +4,7 @@ import moment from 'moment'
 import { useParams } from 'react-router-dom'
 import { getForecastrUrl } from '../utils/urls'
 import { toCelsius } from '../utils/utils'
+import getChartData from '../utils/transform/getChartData'
 import 'moment/locale/es'
 
 
@@ -27,29 +28,8 @@ const useCityPage = () => {
             try{
 
                 const {data} = await axios.get(url)
-                console.log("data", data);
-                const daysAhead = [ 1 , 2, 3, 4, 5]//Se puede solucionar de otra manera viendo 294. contemplamos dia sin datos de la temperatura
-
-                const days = daysAhead.map(d => moment().add(d,'d'))
-                //Forecast chart
-                //{ "dayHour": "Jue 18", "min": 14, "max": 22, },
-                const dataAux = days.map(day => {
-                    const tempObjArray = data.list.filter(item => {
-                        const dayOfYear = moment.unix(item.dt).dayOfYear()
-                        return dayOfYear === day.dayOfYear()
-                    })
-
-                    const temps = tempObjArray.map(item => item.main.temp)
-                    
-                    
-                    
-                    return ({
-                        dayHour: day.format('ddd'),
-                        min: toCelsius(Math.min(...temps)),
-                        max: toCelsius(Math.max(...temps))
-                    })
-                })
-                setChartData(dataAux)
+                
+                setChartData ( getChartData( data ) )
 
 
 
---
+++ src/utils/transform/getChartData.jsx
@@ -0,0 +1,31 @@
+import moment from 'moment'
+import { toCelsius } from '..//utils'
+
+const getChartData = (data) => {
+    console.log("data", data);
+    const daysAhead = [ 1 , 2, 3, 4, 5]//Se puede solucionar de otra manera viendo 294. contemplamos dia sin datos de la temperatura
+    const days = daysAhead.map(d => moment().add(d,'d'))
+
+    //Forecast chart
+    //{ "dayHour": "Jue 18", "min": 14, "max": 22, },
+    const dataAux = days.map(day => {
+
+        const tempObjArray = data.list.filter(item => {
+            const dayOfYear = moment.unix(item.dt).dayOfYear()
+            return dayOfYear === day.dayOfYear()
+        })
+
+        const temps = tempObjArray.map(item => item.main.temp)
+                
+        return ({
+            dayHour: day.format('ddd'),
+            min: toCelsius(Math.min(...temps)),
+            max: toCelsius(Math.max(...temps))
+        })
+
+    })
+
+    return dataAux;
+}
+
+export default getChartData
